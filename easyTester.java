6c62d46712d0d3083be2d6cea395ab2a95becaa1
greta135656 <72942679+greta135656@users.noreply.github.com>
2020-12-17 23:02:56 +0000

    super easy tester

---
+++ easyTester.java
@@ -0,0 +1,16 @@
+public class easyTester {
+
+  public static void main(String[]args) {
+  int[] one = {-100,25,-9,839, 385734, -9348348};
+  Sorts.bubbleSort(one);
+  System.out.println(Arrays.toString(one));
+  int[] two = {0,0,0};
+  Sorts.bubbleSort(two);
+  System.out.println(Arrays.toString(one));
+  int[] three = {three};
+  Sorts.bubbleSort(three);
+  System.out.println(Arrays.toString(one));
+
+  }
+
+}
