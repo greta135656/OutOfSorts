7a64548bdcac9cc2763f2bd10da419ac81011a03
greta135656 <72942679+greta135656@users.noreply.github.com>
2020-12-17 22:45:57 +0000

    compiled

---
+++ Sorts.java
@@ -0,0 +1,21 @@
+public class Sorts{
+  /**Bubble sort of an int array. 
+  *@postcondition The array will be modified such that the elements will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
+
+  
+  public static void bubbleSort(int[] data){ 
+  boolean sorted = false;
+  for(int j = 0; j<data.length-1; j++){
+  sorted = true;
+  for (int i = 0; i < data.length - j-1; i++)
+  if (data[i] > data[i + 1]){
+  sorted = false;
+  int temp = data[i];
+  data[i] = data[i + 1];
+  data[i+1] = temp;
+  }
+  }}
+   
+}
