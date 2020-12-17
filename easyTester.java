701438563137bf2ccff0bdca50ad331558b79111
greta135656 <72942679+greta135656@users.noreply.github.com>
2020-12-17 23:07:57 +0000

    fixed it idk what I did before

--- easyTester.java
+++ easyTester.java
@@ -1,15 +1,17 @@
+import java.util.Arrays;
+
 public class easyTester {
 
   public static void main(String[]args) {
-  int[] one = {-100,25,-9,839, 385734, -9348348};
-  Sorts.bubbleSort(one);
-  System.out.println(Arrays.toString(one));
-  int[] two = {0,0,0};
-  Sorts.bubbleSort(two);
-  System.out.println(Arrays.toString(one));
-  int[] three = {three};
-  Sorts.bubbleSort(three);
-  System.out.println(Arrays.toString(one));
+  int[] number1 = {-100,25,-9,839, 385734, -9348348};
+  Sorts.bubbleSort(number1);
+  System.out.println(Arrays.toString(number1));
+  int[] number2 = {0,0,0};
+  Sorts.bubbleSort(number2);
+  System.out.println(Arrays.toString(number2));
+  int[] number3 = {};
+  Sorts.bubbleSort(number3);
+  System.out.println(Arrays.toString(number3));
 
   }
 
