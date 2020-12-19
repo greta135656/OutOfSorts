1c9a1fa08af788848c620f54a387649377090d84
greta135656 <72942679+greta135656@users.noreply.github.com>
2020-12-19 17:51:33 +0000

    compiled

--- Sorts.java
+++ Sorts.java
@@ -27,18 +27,31 @@ public class Sorts{
  int smallest = data[i];
  for (int j = 0; j < data.length; j++){
  if (smallest > data[j])
- smallest = data[j];
+ smallest = data[j];}
+int temp = data[i];
  data[i] = smallest;
  smallest = data[i];}
  
  }
- }
+ 
+ 
 
  /**insertion sort of an int array. 
 *@postcondition The array will be modified such that the elements will be in increasing order.
 *@param data - the elements to be sorted.
 */
  public static void insertionSort(int[] data){
+ { 
+for (int i = 1; i < data.length; ++i) { 
+int key = data[i]; 
+int j = i - 1; 
+while (j >= 0 && data[j] > key) { 
+data[j + 1] = data[j]; 
+j = j - 1; 
+} 
+data[j + 1] = key; 
+    }
+  } 
  }
    
 }
