0373332769c2330fa119a17dadf36b57b5329775
greta135656 <72942679+greta135656@users.noreply.github.com>
2020-12-25 02:48:01 +0000

    trying to fix bubble

--- Sorts.java
+++ Sorts.java
@@ -11,10 +11,10 @@ public class Sorts{
   sorted = true;
   for (int i = 0; i < data.length - j-1; i++)
   if (data[i] > data[i + 1]){
-  sorted = false;
   int temp = data[i];
   data[i] = data[i + 1];
   data[i+1] = temp;
+  sorted = false;
   }
   }}
 
